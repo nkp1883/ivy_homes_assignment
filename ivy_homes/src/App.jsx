5b0fb298e67ac57c7482a6a8838c771000c2ee28
pandey1883 <justunikk4459@gmail.com>
2026-09-14 10:45:18 +0530

    Initialised React Frontend

---
+++ ivy_homes/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Ivy Homes</h1>
+      <p>Property marketplace frontend</p>
+    </div>
+  )
+}
+
+export default App
---
+++ ivy_homes/src/pages/ListingsDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListingsDetail = () => {
+  return (
+    <div>ListingsDetail</div>
+  )
+}
+
+export default ListingsDetail
